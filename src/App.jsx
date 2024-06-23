3da311bf3c49a5f24ea9c33d9fc3aabe26158b91
vinothsmart <s.vinothkanna90@gmail.com>
2024-06-23 10:47:30 +0530

    chore: Refactor App component to use useCallback hook for count increment

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useCallback, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
@@ -6,8 +6,8 @@ import "./App.css";
 function App() {
   const [count, setCount] = useState(0);
 
-  useEffect(() => {
-    console.log("App mounted", count);
+  const handleCount = useCallback(() => {
+    setCount((prevCount) => prevCount + 1);
   }, []);
 
   return (
@@ -22,9 +22,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+        <button onClick={handleCount}>count is {count}</button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
