fa9637aa3c47e8547f4be2fc2fe4ef013e5e2f98
vinothsmart <s.vinothkanna90@gmail.com>
2024-06-24 12:27:19 +0530

    Add current time display and update header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,32 @@
+import { useState, useEffect, useCallback } from "react";
 import Contact from "./component/Contact";
 import Footer from "./component/Footer";
 import Header from "./component/Header";
 import Hero from "./component/Hero";
 
 const App = () => {
+  const getCurrentTime = useCallback(() => {
+    const date = new Date();
+    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
+  }, []);
+
+  const [currentTime, setCurrentTime] = useState(getCurrentTime());
+
+  useEffect(() => {
+    setInterval(() => {
+      setCurrentTime(getCurrentTime());
+    }, 1000);
+  }, [currentTime]);
+
   return (
     <>
-      <Header />
+      {/* <Header />
       <Hero />
       <Contact />
-      <Footer />
+      <Footer /> */}
+      <h1>{2 + 2}</h1>
+      <h1 style={{ color: "red" }}>Hello Vinoth</h1>
+      <h1>{currentTime}</h1>
     </>
   );
 };
