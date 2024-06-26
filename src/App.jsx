8e2a9efad8449d94afe5cd086b4155b9b5e51b01
vinothsmart <s.vinothkanna90@gmail.com>
2024-06-26 10:23:02 +0530

    Optimize App component rendering and add city list

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,12 @@
-import Contact from "./component/Contact";
-import Footer from "./component/Footer";
-import Header from "./component/Header";
-import Hero from "./component/Hero";
-
 const App = () => {
-  let marks = 81;
+  const cities = ["Karachi", "Lahore", "Islamabad", "Quetta", "Peshawar"];
   return (
     <>
-      {/* <Header />
-      <Hero />
-      <Contact />
-      <Footer /> */}
-      {(() => {
-        if (marks >= 80 && marks <= 100) {
-          return <h1>A+</h1>;
-        } else if (marks >= 70 && marks <= 79) {
-          return <h1>A</h1>;
-        } else if (marks >= 60 && marks <= 69) {
-          return <h1>A-</h1>;
-        } else {
-          return <h1>F</h1>;
-        }
-      })()}
+      <ul>
+        {cities.map((city, index) => (
+          <li key={index.toString()}>{city}</li>
+        ))}
+      </ul>
     </>
   );
 };
