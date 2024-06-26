28dae4fde30c057cc6c770b08455123733552713
vinothsmart <s.vinothkanna90@gmail.com>
2024-06-26 10:39:48 +0530

    refactor: Use LoginButtonStatus component for login status rendering

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
+const LoginButtonStatus = (status) => {
+  return <button>{status ? "Logout" : "Login"}</button>;
+};
+
 const App = () => {
   const status = true;
-
   return (
     <>
       <h1>Login Status</h1>
-      <button>{status ? "Logout" : "Login"}</button>
+      {LoginButtonStatus(status)}
     </>
   );
 };
