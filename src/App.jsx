27b6e5cf1982d7600b35a3cc41f980f1d8791a17
vinothsmart <s.vinothkanna90@gmail.com>
2024-06-26 09:47:41 +0530

    Refactor App component to use conditional rendering for grade display

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,17 @@
-import { useState, useEffect, useCallback } from "react";
 import Contact from "./component/Contact";
 import Footer from "./component/Footer";
 import Header from "./component/Header";
 import Hero from "./component/Hero";
 
 const App = () => {
-  const getCurrentTime = useCallback(() => {
-    const date = new Date();
-    return date.getHours() + ":" + date.getMinutes() + ":" + date.getSeconds();
-  }, []);
-
-  const [currentTime, setCurrentTime] = useState(getCurrentTime());
-
-  useEffect(() => {
-    setInterval(() => {
-      setCurrentTime(getCurrentTime());
-    }, 1000);
-  }, [currentTime]);
-
+  let marks = 79;
   return (
     <>
       {/* <Header />
       <Hero />
       <Contact />
       <Footer /> */}
-      <h1>{2 + 2}</h1>
-      <h1 style={{ color: "red" }}>Hello Vinoth</h1>
-      <h1>{currentTime}</h1>
+      {marks >= 80 ? <h1>Grade A</h1> : <h1>Grade B</h1>}
     </>
   );
 };
