52046b4e703d4689959161e811cc5cfdd9dab01d
vinothsmart <s.vinothkanna90@gmail.com>
2024-06-26 13:12:20 +0530

    Use conditional rendering for login button in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useCallback } from "react";
-import Hero from "./component/Hero";
 
 const App = () => {
   const handleClick = useCallback(() => {
-    alert("Login successful");
+    alert("hai");
   }, []);
   return (
     <>
-      <h1>Login status</h1>
-      <Hero handleClick={handleClick} />
+      <button onClick={handleClick}>Submit</button>
     </>
   );
 };
