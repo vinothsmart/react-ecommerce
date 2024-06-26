bd26ff72c64f4eb9282d451abbbdb2cff0368c88
vinothsmart <s.vinothkanna90@gmail.com>
2024-06-26 16:49:23 +0530

    Refactor App component to use useRef for myHeadline and improve click event handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,14 @@
 import { useRef } from "react";
 
 const App = () => {
-  const myHeadline = useRef();
-  myHeadline.current.innerHTML = "Hi Vinoth using useRef!";
+  let myHeadline = useRef();
 
   const handleClick = () => {
-    myHeadline.current.innerHTML = "Hello Vinoth";
+    myHeadline.innerHTML = "Hello Vinoth";
   };
   return (
     <>
-      <h1 ref={myHeadline}></h1>
+      <h1 ref={(h1) => (myHeadline = h1)}></h1>
       <button onClick={handleClick}>Click me</button>
     </>
   );
