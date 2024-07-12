87c2f5c1ab225acfca808b6df7d80e4534be19da
vinothsmart <s.vinothkanna90@gmail.com>
2024-07-12 17:37:37 +0530

    Refactor App component to use useState hook for names state

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,24 @@
 import { useCallback, useState } from "react";
 
 const App = () => {
-  const [number, setNumber] = useState(0);
+  const [names, setNames] = useState({
+    key1: "value1",
+    key2: "value2",
+    key3: "value3",
+  });
 
-  const handleIncrement = useCallback(() => {
-    setNumber((prev) => prev + 1);
+  const handleChange = useCallback(() => {
+    setNames({
+      key1: "value1 changed",
+      key2: "value2 changed",
+      key3: "value3 changed",
+    });
   }, []);
 
   return (
     <>
-      <h1>Number: {number}</h1>
-      <button onClick={handleIncrement}>Increment</button>
+      <h1>{names.key2}</h1>
+      <button onClick={handleChange}>Change value</button>
     </>
   );
 };
