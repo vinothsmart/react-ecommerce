8f0ea483365c2dff17580b8fd9a3cfcfce472db9
vinothsmart <s.vinothkanna90@gmail.com>
2024-07-23 15:28:04 +0530

    refactor: Use async/await in fetchProductDetail function in App component

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,10 @@ import { useEffect, useState } from "react";
 const App = () => {
   const [product, setProduct] = useState([]);
 
-  const fetchProductDetail = useCallback(() => {
-    fetch("https://dummyjson.com/products/1").then((response) =>
-      response.json().then((data) => {
-        setProduct(data);
-      })
-    );
+  const fetchProductDetail = useCallback(async () => {
+    const result = await fetch("https://dummyjson.com/products/1");
+    const data = await result.json();
+    setProduct(data);
   }, []);
 
   useEffect(() => {
