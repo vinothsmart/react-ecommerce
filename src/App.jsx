db4502456ce8c63440c80c467b5145a12311225d
vinothsmart <s.vinothkanna90@gmail.com>
2024-07-23 13:58:32 +0530

    Refactor form handling in App component and add gender input

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,51 @@
+import { useState } from "react";
+
 const App = () => {
+  const [formData, setFromData] = useState({
+    firstName: "",
+    lastName: "",
+    city: "",
+    gender: "",
+  });
+
   const handleSumbit = (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const data = {};
-    for (let [key, value] of formData.entries()) {
-      data[key] = value;
-    }
-    console.log(data);
   };
 
   return (
-    <div className="container">
+    <div className="container mt-5">
       <form onSubmit={handleSumbit}>
-        <input type="text" name="firstName" placeholder="First Name" />
-        <input type="text" name="lastName" placeholder="Last Name" />
-        <select name="city">
+        <input
+          type="text"
+          name="firstName"
+          placeholder="First Name"
+          value={formData.firstName}
+        />
+        <input
+          type="text"
+          name="lastName"
+          placeholder="Last Name"
+          value={formData.lastName}
+        />
+        <select name="city" value={formData.city}>
           <option value="">Choose City</option>
           <option value="delhi">Delhi</option>
           <option value="mumbai">Mumbai</option>
         </select>
-        <input type="radio" name="gender" value="male" /> Male
-        <input type="radio" name="gender" value="female" /> Female
+        <input
+          checked={formData.gender === "male"}
+          type="radio"
+          name="gender"
+          value="male"
+        />
+        Male
+        <input
+          checked={formData.gender === "female"}
+          type="radio"
+          name="gender"
+          value="female"
+        />
+        Female
         <br />
         <button type="submit">Submit</button>
       </form>
